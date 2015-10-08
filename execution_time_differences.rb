c2276140cc1906397163813132ab0becf873e890
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2015-10-08 10:59:06 -0400

    Add my_min function phase 2

--- execution_time_differences.rb
+++ execution_time_differences.rb
@@ -6,5 +6,15 @@ def my_min(array)
     end
     return first if smallest
   end
+  
   nil
+end
+
+
+def my_min(array)
+  smallest = array.first
+  array.each do |el|
+    smallest = el if el < smallest
+  end
+  smallest
 end
